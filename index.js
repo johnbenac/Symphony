c6b432b58aca6f66342f8fdaf88e94ba93486586
johnbenac <johnbenac@users.noreply.github.com>
2024-04-08 20:13:08 -0400

    2nd commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { getContext } from "../../extensions.js";
+// import { getContext } from "../../extensions.js";
 
-const context = getContext();
-context.chat; // Chat log
-context.characters; // Character list
-context.groups; // Group list
+// const context = getContext();
+// context.chat; // Chat log
+// context.characters; // Character list
+// context.groups; // Group list
 console.log('[!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!]hello world');
 // And many more...
