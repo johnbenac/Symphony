1b89f167a5cff98071413898a253b35acb9357e1
johnbenac <johnbenac@users.noreply.github.com>
2024-04-08 20:09:09 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import { getContext } from "../../extensions.js";
+
+const context = getContext();
+context.chat; // Chat log
+context.characters; // Character list
+context.groups; // Group list
+console.log('[!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!]hello world');
+// And many more...
